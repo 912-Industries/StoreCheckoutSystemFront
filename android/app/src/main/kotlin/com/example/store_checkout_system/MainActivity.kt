32323b8ad333b56a9d48a9ddd8343eea5f6aeaa5
ybueno16 <yuribueno27@gmail.com>
2023-08-12 19:36:51 -0300

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/store_checkout_system/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.store_checkout_system
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ backend/StoreCheckoutSystem/src/main/java/com/mycompany/storecheckoutsystem/StoreCheckoutSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.storecheckoutsystem;
+
+/**
+ *
+ * @author yuri
+ */
+public class StoreCheckoutSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
