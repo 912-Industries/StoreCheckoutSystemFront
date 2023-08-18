b919274c8f7534a5727cef4c4884677aaf4f3fe8
ybueno16 <yuribueno27@gmail.com>
2023-08-18 15:22:36 -0300

    Implementação Tabela Estoque

--- lib/screens/home/estoque.dart
+++ lib/screens/home/estoque.dart
@@ -1,31 +1,71 @@
 import 'package:flutter/material.dart';
+import '/services/estoque_service.dart';
 
-class EstoquePage extends StatelessWidget {
+class EstoquePage extends StatefulWidget {
+  @override
+  _EstoquePageState createState() => _EstoquePageState();
+}
+
+class _EstoquePageState extends State<EstoquePage> {
+  final EstoqueService produtoService = EstoqueService();
+  List<Map<String, dynamic>>? produtos;
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Padding(
-        padding: EdgeInsets.all(20),
-        child: Table(
-          border: TableBorder.all(),
-          children: [
-            TableRow(children: [
-              Text('ID'),
-              Text('Nome'),
-              Text('Preco'),
-              Text('Categoria')
-            ]),
-            TableRow(children: [
-              Text('2011'),
-              Text('Dart'),
-              Text('Lars Bak'),
-            ]),
-            TableRow(children: [
-              Text('1996'),
-              Text('Java'),
-              Text('James Gosling'),
-            ]),
-          ],
+        padding: EdgeInsets.all(100),
+        child: FutureBuilder<List<Map<String, dynamic>>>(
+          future: produtoService.fetchProdutos(),
+          builder: (BuildContext context,
+              AsyncSnapshot<List<Map<String, dynamic>>> snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return CircularProgressIndicator();
+            } else if (snapshot.hasError) {
+              return Text('Error: ${snapshot.error}');
+            } else {
+              produtos = snapshot.data;
+              return DataTable(
+                columns: const <DataColumn>[
+                  DataColumn(
+                    label: Text(
+                      'Id',
+                    ),
+                  ),
+                  DataColumn(
+                    label: Text(
+                      'Nome',
+                    ),
+                  ),
+                  DataColumn(
+                    label: Text(
+                      'Preço',
+                    ),
+                  ),
+                  DataColumn(
+                    label: Text(
+                      'Categoria',
+                    ),
+                  ),
+                ],
+                rows: produtos!
+                    .map((produto) => DataRow(
+                          cells: <DataCell>[
+                            DataCell(Text(produto['id_produto']?.toString() ??
+                                '')), // Atualizado para 'id_produto'
+                            DataCell(Text(produto['nome_produto'] ??
+                                '')), // Atualizado para 'nome_produto'
+                            DataCell(Text(
+                                produto['preco_produto']?.toString() ??
+                                    '')), // Atualizado para 'preco_produto'
+                            DataCell(Text(produto['categoria_produto'] ??
+                                '')), // Atualizado para 'categoria_produto' // Atualizado para 'descricao_produto'
+                          ],
+                        ))
+                    .toList(),
+              );
+            }
+          },
         ),
       ),
     );
---
+++ lib/services/estoque_service.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class EstoqueService {
+  Future<List<Map<String, dynamic>>> fetchProdutos() async {
+    final response =
+        await http.get(Uri.parse('http://localhost:8080/api/produto'));
+
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
+
+    if (response.statusCode == 200) {
+      List jsonResponse = json.decode(response.body);
+      return jsonResponse.map((item) => item as Map<String, dynamic>).toList();
+    } else {
+      throw Exception('Falha ao carregar produtos');
+    }
+  }
+}
