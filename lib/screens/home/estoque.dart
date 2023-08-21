3d06dca16ec5651aebd74769a09aef3bbee40c57
ybueno16 <yuribueno27@gmail.com>
2023-08-21 15:20:32 -0300

    Implementação pesquisa de produto

--- lib/screens/home/estoque.dart
+++ lib/screens/home/estoque.dart
@@ -9,63 +9,106 @@ class EstoquePage extends StatefulWidget {
 class _EstoquePageState extends State<EstoquePage> {
   final EstoqueService produtoService = EstoqueService();
   List<Map<String, dynamic>>? produtos;
+  String query = '';
 
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Padding(
         padding: EdgeInsets.all(100),
-        child: FutureBuilder<List<Map<String, dynamic>>>(
-          future: produtoService.fetchProdutos(),
-          builder: (BuildContext context,
-              AsyncSnapshot<List<Map<String, dynamic>>> snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return CircularProgressIndicator();
-            } else if (snapshot.hasError) {
-              return Text('Error: ${snapshot.error}');
-            } else {
-              produtos = snapshot.data;
-              return DataTable(
-                columns: const <DataColumn>[
-                  DataColumn(
-                    label: Text(
-                      'Id',
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                width: MediaQuery.of(context).size.width *
+                    0.4, // 80% of screen width
+                child: TextField(
+                  onChanged: (value) {
+                    setState(() {
+                      query = value;
+                    });
+                  },
+                  decoration: InputDecoration(
+                    hintText: "Pesquisar",
+                    prefixIcon: Icon(Icons.search),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(25.0)),
                     ),
                   ),
-                  DataColumn(
-                    label: Text(
-                      'Nome',
-                    ),
-                  ),
-                  DataColumn(
-                    label: Text(
-                      'Preço',
-                    ),
-                  ),
-                  DataColumn(
-                    label: Text(
-                      'Categoria',
-                    ),
-                  ),
-                ],
-                rows: produtos!
-                    .map((produto) => DataRow(
-                          cells: <DataCell>[
-                            DataCell(Text(produto['id_produto']?.toString() ??
-                                '')), // Atualizado para 'id_produto'
-                            DataCell(Text(produto['nome_produto'] ??
-                                '')), // Atualizado para 'nome_produto'
-                            DataCell(Text(
-                                produto['preco_produto']?.toString() ??
-                                    '')), // Atualizado para 'preco_produto'
-                            DataCell(Text(produto['categoria_produto'] ??
-                                '')), // Atualizado para 'categoria_produto' // Atualizado para 'descricao_produto'
-                          ],
-                        ))
-                    .toList(),
-              );
-            }
-          },
+                ),
+              ),
+              FutureBuilder<List<Map<String, dynamic>>>(
+                future: produtoService.fetchProdutos(),
+                builder: (BuildContext context,
+                    AsyncSnapshot<List<Map<String, dynamic>>> snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return CircularProgressIndicator();
+                  } else if (snapshot.hasError) {
+                    return Text('Error: ${snapshot.error}');
+                  } else {
+                    produtos = snapshot.data;
+                    return SingleChildScrollView(
+                      scrollDirection: Axis.vertical,
+                      child: DataTable(
+                        columns: const <DataColumn>[
+                          DataColumn(
+                            label: Text(
+                              'Id',
+                            ),
+                          ),
+                          DataColumn(
+                            label: Text(
+                              'Nome',
+                            ),
+                          ),
+                          DataColumn(
+                            label: Text(
+                              'Preço',
+                            ),
+                          ),
+                          DataColumn(
+                            label: Text(
+                              'Categoria',
+                            ),
+                          ),
+                        ],
+                        rows: List<DataRow>.from(
+                          produtos!
+                              .where((produto) =>
+                                  produto['nome_produto']
+                                      .toLowerCase()
+                                      .contains(query.toLowerCase()) ||
+                                  produto['id_produto']
+                                      .toString()
+                                      .toLowerCase()
+                                      .contains(query.toLowerCase()) ||
+                                  produto['categoria_produto']
+                                      .toLowerCase()
+                                      .contains(query.toLowerCase()))
+                              .map((produto) => DataRow(
+                                    cells: <DataCell>[
+                                      DataCell(Text(produto['id_produto']
+                                              ?.toString() ??
+                                          '')), // Atualizado para 'id_produto'
+                                      DataCell(Text(produto['nome_produto'] ??
+                                          '')), // Atualizado para 'nome_produto'
+                                      DataCell(Text(produto['preco_produto']
+                                              ?.toString() ??
+                                          '')), // Atualizado para 'preco_produto'
+                                      DataCell(Text(produto[
+                                              'categoria_produto'] ??
+                                          '')), // Atualizado para 'categoria_produto' // Atualizado para 'descricao_produto'
+                                    ],
+                                  ))
+                              .toList(),
+                        ),
+                      ),
+                    );
+                  }
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
