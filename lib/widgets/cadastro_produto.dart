4e94019f1183d1935abf3e6b7f0e5b2a9e7af677
ybueno16 <yuribueno27@gmail.com>
2023-08-30 16:13:44 -0300

    Cadastro Produto

--- lib/widgets/cadastro_produto.dart
+++ lib/widgets/cadastro_produto.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class CadastroProduto extends StatefulWidget{
+class CadastroProduto extends StatefulWidget {
   @override
   _CadastroProduto createState() => _CadastroProduto();
 }
@@ -8,27 +8,66 @@ class CadastroProduto extends StatefulWidget{
 class _CadastroProduto extends State<CadastroProduto> {
   @override
   Widget build(BuildContext context) {
+    var nomeProdutoController;
+    var descricaoProdutoController;
+    var precoProdutoController;
     return MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Flutter Layouts'),
-          ),
-          body: Column(
-            children: const [
-              Center(
-                heightFactor: 2,
-                child: Text(
-                  'Olá Mundo!',
-                  textDirection: TextDirection.ltr,
-                  style: TextStyle(
-                    fontSize: 32,
-                  color: Colors.black,
-                  )
-                ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Cadastro de Produto'),
+        ),
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.4,
+                    child: TextFormField(
+                      controller: nomeProdutoController,
+                      decoration: InputDecoration(
+                        labelText: 'Nome do Produto',
+                        prefixIcon: Padding(
+                          child: Icon(Icons.shopping_bag),
+                          padding: EdgeInsets.all(10),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.4,
+                    child: TextFormField(
+                      controller: descricaoProdutoController,
+                      decoration: InputDecoration(
+                        labelText: 'Descrição do Produto',
+                        prefixIcon: Padding(
+                          child: Icon(Icons.description),
+                          padding: EdgeInsets.all(10),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.4,
+                    child: TextFormField(
+                      controller: precoProdutoController,
+                      decoration: InputDecoration(
+                        labelText: 'Preço do Produto',
+                        prefixIcon: Padding(
+                          child: Icon(Icons.attach_money),
+                          padding: EdgeInsets.all(10),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ]
+            ),
           ),
         ),
+      ),
     );
   }
 }
