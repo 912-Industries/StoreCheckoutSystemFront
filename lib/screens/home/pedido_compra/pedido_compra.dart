ada8b96df06e6aa3b91d5438c1000b145fc82a24
Yuri Bueno <yuribueno27@gmail.com>
2023-11-04 14:15:25 -0300

    Fix quantidades
    Foi corrigido cadastrar a quantidade através do campo

--- lib/screens/home/pedido_compra/pedido_compra.dart
+++ lib/screens/home/pedido_compra/pedido_compra.dart
@@ -15,15 +15,14 @@ class _PedidoCompra extends State<PedidoCompraPage> {
   final descricaoProdutoController = TextEditingController();
   final precoProdutoController = TextEditingController();
   final categoriaProdutoController = TextEditingController();
-  late TextEditingController quantidadeProdutoController;
+  var quantidadeProdutoController = TextEditingController();
+
   int quantidade = 1;
 
   @override
   void initState() {
     super.initState();
-    quantidadeProdutoController =
-        TextEditingController(text: quantidade.toString());
-    quantidadeProdutoController.text = quantidade.toString();
+    quantidadeProdutoController = TextEditingController(text: '1');
   }
 
   @override
@@ -64,181 +63,186 @@ class _PedidoCompra extends State<PedidoCompraPage> {
             padding: const EdgeInsets.symmetric(horizontal: 20),
             child: Center(
               child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Container(
-                      width: MediaQuery.of(context).size.width * 0.4,
-                      child: TextFormField(
-                        controller: nomeProdutoController,
-                        decoration: InputDecoration(
-                          labelText: 'Nome do Produto',
-                          prefixIcon: Padding(
-                            child: Icon(Icons.shopping_bag),
-                            padding: EdgeInsets.all(5),
-                          ),
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.4,
+                    child: TextFormField(
+                      controller: nomeProdutoController,
+                      decoration: InputDecoration(
+                        labelText: 'Nome do Produto',
+                        prefixIcon: Padding(
+                          child: Icon(Icons.shopping_bag),
+                          padding: EdgeInsets.all(5),
                         ),
                       ),
                     ),
-                    Container(
-                      width: MediaQuery.of(context).size.width * 0.4,
-                      child: TextFormField(
-                        controller: precoProdutoController,
-                        keyboardType: TextInputType.number,
-                        inputFormatters: <TextInputFormatter>[
-                          CurrencyTextInputFormatter(
-                            locale: 'pt-BR',
-                            decimalDigits: 2,
-                            symbol: 'R\$ ',
-                          ),
-                        ],
-                        decoration: InputDecoration(
-                          labelText: 'Preço de Custo',
-                          prefixIcon: Padding(
-                            child: Icon(Icons.attach_money),
-                            padding: const EdgeInsets.all(10),
-                          ),
-                        ),
-                      ),
-                    ),
-                    Container(
-                      width: MediaQuery.of(context).size.width * 0.4,
-                      child: TextFormField(
-                        controller: categoriaProdutoController,
-                        decoration: InputDecoration(
-                          labelText: 'Categoria do Produto',
-                          prefixIcon: Padding(
-                            child: Icon(Icons.category_rounded),
-                            padding: EdgeInsets.all(5),
-                          ),
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width * 0.4,
-                      child: TextField(
-                        textAlignVertical: TextAlignVertical.top,
-                        controller: descricaoProdutoController,
-                        maxLines: null,
-                        maxLength: 255,
-                        maxLengthEnforcement: MaxLengthEnforcement.enforced,
-                        decoration: InputDecoration(
-                          labelText: 'Descrição do Produto',
-                          prefixIcon: Padding(
-                            child: Icon(Icons.description),
-                            padding: const EdgeInsets.all(10),
-                          ),
-                        ),
-                        buildCounter: (
-                          context, {
-                          required int currentLength,
-                          required int? maxLength,
-                          required bool isFocused,
-                        }) {
-                          return DefaultTextStyle(
-                            style: TextStyle(color: Colors.grey),
-                            child: Text(
-                              '$currentLength/$maxLength caracteres',
-                              semanticsLabel: 'contador caracteres',
-                            ),
-                          );
-                        },
-                      ),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Text('Quantidade: '),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        IconButton(
-                          onPressed: diminuirQuantidade,
-                          icon: Icon(
-                            Icons.remove_circle_outlined,
-                            color: Colors.green,
-                          ),
-                        ),
-                        Container(
-                          width: 50.0, // ajuste este valor conforme necessário
-                          child: TextFormField(
-                            controller: quantidadeProdutoController,
-                            keyboardType: TextInputType.number,
-                            textAlign: TextAlign.center,
-                            decoration: InputDecoration(
-                              border: InputBorder.none,
-                              contentPadding:
-                                  EdgeInsets.symmetric(vertical: 0.0),
-                            ),
-                          ),
-                        ),
-                        IconButton(
-                          icon: Icon(
-                            Icons.add_circle_rounded,
-                            color: Colors.green,
-                          ),
-                          onPressed: aumentarQuantidade,
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.4,
+                    child: TextFormField(
+                      controller: precoProdutoController,
+                      keyboardType: TextInputType.number,
+                      inputFormatters: <TextInputFormatter>[
+                        CurrencyTextInputFormatter(
+                          locale: 'pt-BR',
+                          decimalDigits: 2,
+                          symbol: 'R\$ ',
                         ),
                       ],
+                      decoration: InputDecoration(
+                        labelText: 'Preço de Custo',
+                        prefixIcon: Padding(
+                          child: Icon(Icons.attach_money),
+                          padding: const EdgeInsets.all(10),
+                        ),
+                      ),
                     ),
-                    SizedBox(
-                      height: 20,
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.4,
+                    child: TextFormField(
+                      controller: categoriaProdutoController,
+                      decoration: InputDecoration(
+                        labelText: 'Categoria do Produto',
+                        prefixIcon: Padding(
+                          child: Icon(Icons.category_rounded),
+                          padding: EdgeInsets.all(5),
+                        ),
+                      ),
                     ),
-                    Container(
-                        width: MediaQuery.of(context).size.width * 0.2,
-                        height: MediaQuery.of(context).size.height * 0.060,
-                        child: ElevatedButton(
-                          onPressed: () async {
-                            CadastroProdutoService service =
-                                CadastroProdutoService();
-
-                            String precoProdutoText =
-                                precoProdutoController.text;
-                            precoProdutoText =
-                                precoProdutoText.replaceAll('R\$', '');
-                            precoProdutoText =
-                                precoProdutoText.replaceAll(',', '.');
-
-                            double precoProduto;
-                            quantidade;
-                            try {
-                              precoProduto = double.parse(precoProdutoText);
-                            } catch (e) {
-                              print(
-                                  'Não foi possível converter a string para um double: $e');
-                              return;
-                            }
-
-                            bool? isValid = await service.cadastroProduto(
-                                nomeProdutoController.text,
-                                precoProduto,
-                                categoriaProdutoController.text,
-                                descricaoProdutoController.text,
-                                quantidade);
-
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.4,
+                    child: TextField(
+                      textAlignVertical: TextAlignVertical.top,
+                      controller: descricaoProdutoController,
+                      maxLines: null,
+                      maxLength: 255,
+                      maxLengthEnforcement: MaxLengthEnforcement.enforced,
+                      decoration: InputDecoration(
+                        labelText: 'Descrição do Produto',
+                        prefixIcon: Padding(
+                          child: Icon(Icons.description),
+                          padding: const EdgeInsets.all(10),
+                        ),
+                      ),
+                      buildCounter: (
+                        context, {
+                        required int currentLength,
+                        required int? maxLength,
+                        required bool isFocused,
+                      }) {
+                        return DefaultTextStyle(
+                          style: TextStyle(color: Colors.grey),
+                          child: Text(
+                            '$currentLength/$maxLength caracteres',
+                            semanticsLabel: 'contador caracteres',
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text('Quantidade: '),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      IconButton(
+                        onPressed: diminuirQuantidade,
+                        icon: Icon(
+                          Icons.remove_circle_outlined,
+                          color: Colors.green,
+                        ),
+                      ),
+                      Container(
+                        width: 50.0, // ajuste este valor conforme necessário
+                        child: TextFormField(
+                          controller: quantidadeProdutoController,
+                          keyboardType: TextInputType.number,
+                          textAlign: TextAlign.center,
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            contentPadding: EdgeInsets.symmetric(vertical: 0.0),
+                          ),
+                          onChanged: (value) {
                             setState(() {
-                              limpaCampos();
+                              quantidade = int.parse(value);
                             });
-
-                            if (isValid != null && isValid) {
-                              EstoquePage.shouldRefreshData.value =
-                                  !EstoquePage.shouldRefreshData.value;
-                              ElegantNotification.success(
-                                title: Text("Pedido de Compra de Produto"),
-                                description: Text(
-                                    "O pedido de compra foi contabilizado com sucesso"),
-                              ).show(context);
-                            } else {
-                              ElegantNotification.error(
-                                      title:
-                                          Text("Pedido de Compra de Produto"),
-                                      description: Text(
-                                          "Ocorreu algum erro ao contabilizar o pedido de compra"))
-                                  .show(context);
-                            }
                           },
-                          child: Text('Pedido de Compra de Produto'),
-                        )),
-                  ]),
+                        ),
+                      ),
+                      IconButton(
+                        icon: Icon(
+                          Icons.add_circle_rounded,
+                          color: Colors.green,
+                        ),
+                        onPressed: aumentarQuantidade,
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.2,
+                    height: MediaQuery.of(context).size.height * 0.060,
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        CadastroProdutoService service =
+                            CadastroProdutoService();
+
+                        String precoProdutoText = precoProdutoController.text;
+                        precoProdutoText =
+                            precoProdutoText.replaceAll('R\$', '');
+                        precoProdutoText =
+                            precoProdutoText.replaceAll(',', '.');
+
+                        double precoProduto;
+                        int quantidade =
+                            int.parse(quantidadeProdutoController.text);
+                        try {
+                          precoProduto = double.parse(precoProdutoText);
+                        } catch (e) {
+                          print(
+                              'Não foi possível converter a string para um double: $e');
+                          return;
+                        }
+
+                        bool? isValid = await service.cadastroProduto(
+                            nomeProdutoController.text,
+                            precoProduto,
+                            categoriaProdutoController.text,
+                            descricaoProdutoController.text,
+                            quantidade);
+
+                        setState(() {
+                          limpaCampos();
+                        });
+
+                        if (isValid != null && isValid) {
+                          EstoquePage.shouldRefreshData.value =
+                              !EstoquePage.shouldRefreshData.value;
+                          ElegantNotification.success(
+                            title: Text("Pedido de Compra de Produto"),
+                            description: Text(
+                                "O pedido de compra foi contabilizado com sucesso"),
+                          ).show(context);
+                        } else {
+                          ElegantNotification.error(
+                            title: Text("Pedido de Compra de Produto"),
+                            description: Text(
+                                "Ocorreu algum erro ao contabilizar o pedido de compra"),
+                          ).show(context);
+                        }
+                      },
+                      child: Text('Pedido de Compra de Produto'),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
