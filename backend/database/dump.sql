06797404fe31e1a7fa358f91302dfdb9ca2b456b
ybueno16 <yuribueno27@gmail.com>
2023-11-07 15:13:47 -0300

    update dump sql

--- backend/database/dump.sql
+++ backend/database/dump.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.32, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 8.1.0, for Linux (x86_64)
 --
 -- Host: localhost    Database: StoreCheckout
 -- ------------------------------------------------------
--- Server version	8.0.32
+-- Server version	8.1.0
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,6 +16,33 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `markup`
+--
+
+DROP TABLE IF EXISTS `markup`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `markup` (
+  `id_markup` int NOT NULL AUTO_INCREMENT,
+  `despesaFixa_markup` int DEFAULT NULL,
+  `despesaVariaveis_markup` int DEFAULT NULL,
+  `margemLucro_markup` decimal(10,2) DEFAULT NULL,
+  `resultadoMarkup_markup` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`id_markup`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `markup`
+--
+
+LOCK TABLES `markup` WRITE;
+/*!40000 ALTER TABLE `markup` DISABLE KEYS */;
+INSERT INTO `markup` VALUES (1,10,12,20.00,0.00),(2,12,10,20.00,1.72),(3,12,10,20.00,1.72),(4,12,8,20.00,1.67),(5,12,8,20.00,1.67),(6,8,12,20.00,1.67),(7,8,12,20.00,1.67);
+/*!40000 ALTER TABLE `markup` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `produto`
 --
 
@@ -23,13 +50,15 @@ DROP TABLE IF EXISTS `produto`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `produto` (
-  `id_produto` int NOT NULL,
+  `id_produto` int NOT NULL AUTO_INCREMENT,
   `nome_produto` varchar(255) DEFAULT NULL,
-  `preco_produto` decimal(10,2) DEFAULT NULL,
+  `precoFinal_produto` decimal(10,2) NOT NULL,
   `categoria_produto` varchar(50) DEFAULT NULL,
   `descricao_produto` text,
+  `quantidade_produto` int NOT NULL,
+  `precoCusto_produto` decimal(10,2) NOT NULL,
   PRIMARY KEY (`id_produto`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=45 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,7 +67,7 @@ CREATE TABLE `produto` (
 
 LOCK TABLES `produto` WRITE;
 /*!40000 ALTER TABLE `produto` DISABLE KEYS */;
-INSERT INTO `produto` VALUES (1,'Produto A',49.99,'Eletrnicos','Um produto eletrnico de alta qualidade.'),(2,'Produto B',29.99,'Moda','Uma pea de moda elegante para todas as ocasies.'),(3,'Produto C',9.99,'Casa','Um item essencial para qualquer lar.'),(4,'Produto D',149.99,'Eletrnicos','Recursos avanados e desempenho excepcional.'),(5,'Produto E',5.49,'Alimentos','Um lanche delicioso para satisfazer seus desejos.');
+INSERT INTO `produto` VALUES (40,'te',279.44,'te','te',1,167.33),(42,'12',167.33,'123','321',1,100.20),(43,'123',167.33,'312','123',1,100.20),(44,'test',150.30,'teste','teste',5,90.00);
 /*!40000 ALTER TABLE `produto` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -78,4 +107,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-08-18 13:35:34
+-- Dump completed on 2023-11-07 18:12:57
